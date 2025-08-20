cb336d079d05ca57a9b6695a73314963f6cc6cde
YourLogic7 <m.r.izky.nash@gmail.com>
2025-08-20 10:36:21 +0700

    bismilah kita coba

---
+++ script.js
@@ -0,0 +1,10 @@
+function tampilkanData() {
+  const nama = document.getElementById('nama').value;
+  const email = document.getElementById('email').value;
+  const hasilDiv = document.getElementById('hasil');
+
+  hasilDiv.innerHTML = `
+    <p>Nama: ${nama}</p>
+    <p>Email: ${email}</p>
+  `;
+}
