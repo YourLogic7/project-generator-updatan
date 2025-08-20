b4c1beab624f0191ce9dd3a33516ceb7201b155e
YourLogic7 <m.r.izky.nash@gmail.com>
2025-08-20 16:53:41 +0700

    keluarin data hasil radio

--- script.js
+++ script.js
@@ -3,18 +3,19 @@ let dataTersimpan = [];
 function tampilkanData() {
   const perner = document.getElementById('perner').value;
   const dsc = document.getElementById('dsc').value;
-  const insera = document.getElementById('insera').value = "";
-  const headline = document.getElementById('headline').value = "";
-  const pelanggan = document.getElementById('pelanggan').value = "";
-  const cp = document.getElementById('cp').value = "";
-  const layanan = document.getElementById('layanan').value = "";
-  const resume = document.getElementById('resume').value = "";
-  const alamat = document.getElementById('alamat').value = "";
-  const pengecekan = document.getElementById('pengecekan').value= "";
-  const jabatan = document.getElementById('jabatan').value = "";
-  const respon = document.getElementById('respon').value = "";
-  const carring = document.getElementById('carring').value = "";
-  const jam = document.getElementById('jam').value = "";
+  const insera = document.getElementById('insera').value;
+  const headline = document.getElementById('headline').value;
+  const pelanggan = document.getElementById('pelanggan').value;
+  const cp = document.getElementById('cp').value;
+  const layanan = document.getElementById('layanan').value;
+  const resume = document.getElementById('resume').value;
+  const alamat = document.getElementById('alamat').value;
+  const pengecekan = document.getElementById('pengecekan').value;
+  const jabatan = document.getElementById('jabatan').value;
+  let pilihanNok = document.querySelector('input[name="nok"]:checked');
+  let pilihanOk = document.querySelector('input[name="ok"]:checked');
+  const carring = document.getElementById('carring').value;
+  const jam = document.getElementById('jam').value;
 
   // buat reset form
   document.getElementById("dsc").value = "";
@@ -30,17 +31,15 @@ function tampilkanData() {
   document.getElementById('respon').value = "";
   document.getElementById('carring').value = "";
   document.getElementById('jam').value = "";
-  
+
   // menampilkan hasil ini mah
   const hasilDiv = document.getElementById('hasil');
 
   
   // Menampilkan data yang diinputkan
   hasilDiv.innerHTML = `
-    <p>${dsc}${insera}</p>
-    <p>${perner}</p>
-    <p>Nama: ${headline}</p>
-    <p>Email: ${pelanggan}</p>
-    
+    <p>${dsc} ${insera}</p>
+    <p>${perner} / C4 Area / ${jabatan} / Hasil Cek: ${pengecekan}</p>
+  
   `;
 }
