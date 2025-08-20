3f0a157f45a375aece7d6a788702e69881985c52
YourLogic7 <m.r.izky.nash@gmail.com>
2025-08-20 15:38:29 +0700

    udah agak lengkap

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
+let dataTersimpan = [];
+
 function tampilkanData() {
-  const nama = document.getElementById('nama').value;
-  const email = document.getElementById('email').value;
+  const perner = document.getElementById('perner').value;
+  const dsc = document.getElementById('dsc').value;
+  const insera = document.getElementById('insera').value = "";
+  const headline = document.getElementById('headline').value = "";
+  const pelanggan = document.getElementById('pelanggan').value = "";
+  const cp = document.getElementById('cp').value = "";
+  const layanan = document.getElementById('layanan').value = "";
+  const resume = document.getElementById('resume').value = "";
+  const alamat = document.getElementById('alamat').value = "";
+  const pengecekan = document.getElementById('pengecekan').value= "";
+  const jabatan = document.getElementById('jabatan').value = "";
+  const respon = document.getElementById('respon').value = "";
+  const carring = document.getElementById('carring').value = "";
+  const jam = document.getElementById('jam').value = "";
+
   const hasilDiv = document.getElementById('hasil');
 
+  // Menampilkan data yang diinputkan
   hasilDiv.innerHTML = `
-    <p>Nama: ${nama}</p>
-    <p>Email: ${email}</p>
+    <p>${dsc} + ${insera}</p>
+    <p>${perner}</p>
+    <p>Nama: ${headline}</p>
+    <p>Email: ${pelanggan}</p>
     
   `;
 }
